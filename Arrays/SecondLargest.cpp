44da5caf2dda929a758a7372cc2ff4db22fa4a87
mohanburgula <mohanburgula98@gmail.com>
2026-07-22 21:38:36 +0530

    Updated with TC Betterment SecondLargest.cpp
    Removed commented-out code for finding the second largest number.

--- Arrays/SecondLargest.cpp
+++ Arrays/SecondLargest.cpp
@@ -6,15 +6,6 @@ public:
         sort(nums.begin(),nums.end());
         int maxi=nums[nums.size()-1];
         int secondLargest=-1;
-        // for(int i=0;i<n;i++)
-        // {
-        //     if(nums[i]>secondLargest && nums[i]!= maxi)
-        //     {
-        //         secondLargest=nums[i];
-        //         // break;
-        //     }
-        // }
-
         for(int i=n-2;i>=0;i--)
         {
             if(maxi!=nums[i])
