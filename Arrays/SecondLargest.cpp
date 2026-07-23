d9f4fb363ec231327c80fd2145a08b96ce817a51
mohanburgula <mohanburgula98@gmail.com>
2026-07-23 08:09:26 +0530

    NEW second largest element in array
    Implemented a function to find the second largest element in an array by first identifying the largest element and then comparing the rest.

--- Arrays/SecondLargest.cpp
+++ Arrays/SecondLargest.cpp
@@ -1,4 +1,24 @@
+//1.  FIRST FIND THE LARGEST ELEMENT USING THE MAX FUNCTION
+//2. AND COMPARE THE MAX ELEMENT WITH THE NEW VARIABLE NAMED SECOND LARGEST 
+// 3. STORE THE VALUE IF IT IS GREATER THAN SECOND LARGEST BUT NOT THE LARGES BECAUSE WE NEED TO FIND SECOND LARGEST NOT LARGEST
 class Solution {
+  public:
+    int getSecondLargest(vector<int> &arr) {
+        // code here
+        int maxi=*max_element(arr.begin(),arr.end());
+        int secondlarge=-1;
+        for(int i=0;i<arr.size();i++)
+        {
+            if(arr[i]>secondlarge&&arr[i]!=maxi)
+            {
+               secondlarge=arr[i]; 
+            }
+        }
+        return secondlarge;
+    }
+};
+
+/*class Solution {
 public:
     int secondLargestElement(vector<int>& nums) {
         //your code goes here
@@ -18,3 +38,4 @@ public:
       
     }
 };
+*/
